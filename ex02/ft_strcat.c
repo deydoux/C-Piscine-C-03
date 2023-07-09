f65ed99c66c4a4b6bdd74e653125945dcacf8911
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-09 17:13:52 +0200

    Fix norm: ex02

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -6,14 +6,14 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 15:10:37 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/09 17:10:23 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/09 17:12:43 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strlen(char *str)
 {
 	int	i;
-	
+
 	i = 0;
 	while (str[i] != '\0')
 		i++;
