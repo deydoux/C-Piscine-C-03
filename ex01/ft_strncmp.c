24682195356526d77c741a70eb5e18a91abc96e5
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-11 19:44:33 +0200

    Fix ex01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 12:00:11 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/09 15:07:27 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/11 19:38:17 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	if (s1[i] == s2[i])
-		return (0);
-	else
-		return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
