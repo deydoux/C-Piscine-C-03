09dc5158d126ed0bf5822427913bff2acc6463ad
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-07-11 20:54:23 +0200

    Fix ex05

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 15:10:37 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/10 12:30:54 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/11 20:51:26 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	end;
 
 	end = ft_strlen(dest);
-	if (size < end - 1)
-		return (end);
+	if (size <= end)
+		return (ft_strlen(src) + size);
 	i = 0;
-	while (i < size - end - 1 && src[i] != '\0')
+	while (i + end < size - 1 && src[i] != '\0')
 	{
 		dest[end + i] = src[i];
 		i++;
 	}
 	dest[end + i] = '\0';
-	return (end + i);
+	return (end + ft_strlen(src));
 }
